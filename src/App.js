ef6b19812c81e419674bf1b81b25cef766945431
veldakarimi <veldakarimi@gmail.com>
2021-06-26 07:49:40 +0300

    interface: hot fix

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ const App = () => {
   return (
     <div className="App h-screen flex flex-col justify-center from-purple to-pink  bg-gradient-to-br w-full sm:mx-auto">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="max-w-lg rounded-lg box-content bg-white shadow-inner-lg py-32 px-28 space-y-4 sm:mx-auto">
+        <div className="max-w-lg rounded-lg box-content bg-white shadow-inner-lg py-32 px-28 space-y-3 sm:mx-auto">
               <p className="font-sans text-2xl -mt-24"> Upload your image</p> 
-              <p className="font-sans text-sm space-y-5 px-3 py-7"> File should be jpeg, png..</p>
-              <div className="border-dotted border-4 -m-16 border-purple py-48 px-44"> 
-              <img src={upload_image} alt="upload"></img>
-              <p className="font-sans text-base "> Drag and Drop your images here</p>
+              <p className="font-sans text-sm space-y-4 px-3 py-5"> File should be jpeg, png..</p>
+              <div className="border-dotted border-4 -m-16 border-purple py-20 px-28"> 
+              <img src={upload_image} alt="upload" className="  transform scale-150"></img> <br/>
+              <p className="font-sans text-base py-8 -mx-12"> Drag and Drop your images here </p>
+              
+
               </div>
             </div>
 
